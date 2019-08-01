51f8e388cabe7458250b895bef5c048b7d601567
iplayguitar81 <iplayguitar81@yahoo.com>
2019-07-31 21:41:17 -0700

    Changed navbar branding title...

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,7 +1,6 @@
 <template>
 
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-danger mb-2"><a href="/weather" class="navbar-brand text-white">Laravel/Vue Challenge</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger mb-2"><a href="/weather" class="navbar-brand text-white">Laravel/Vue Weather SPA</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -25,7 +24,6 @@
 
 
 <script>
-
 
     export default {
         name: 'Navbar',}
--- resources/views/component-vue.blade.php
+++ resources/views/component-vue.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Vue/Laravel Weather Report cSnyder</title>
+    <title>Laravel/Vue Weather Report</title>
     <meta name="csrf-token" content="{{csrf_token()}}">
     <script>window.Laravel = {csrfToken: '{{csrf_token()}}'}</script>
 
